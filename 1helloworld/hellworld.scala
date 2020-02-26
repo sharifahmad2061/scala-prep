3ff167c283628a45506e2a3a44001724a7fdb13b
sharif ahmad <sharifahmad2061@gmail.com>
2020-02-26 11:38:43 +0500

    converted person var parameters to val parameters, which makes them const and unchangeable

--- 1helloworld/hellworld.scala
+++ 1helloworld/hellworld.scala
@@ -1,10 +1,10 @@
-class Person(var firstName: String, var lastName: String) {
+class Person(val firstName: String, val lastName: String) {
     def printFullName() = println(s"$firstName $lastName")
 }
 object Hello extends App {
     val person = new Person("sharif", "ahmad")
     person.printFullName()
-    person.firstName = "Tahir"
+    // person.firstName = "Tahir"
     person.printFullName()
     println("Hello, world")
 }
