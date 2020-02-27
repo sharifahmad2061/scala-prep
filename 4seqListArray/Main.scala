c4f23972b775d04e26b760da9fbfc91ed131311b
sharif ahmad <sharifahmad2061@gmail.com>
2020-02-27 19:03:18 +0500

    learned about Seq and List and how they are preferred over Array

---
+++ 4seqListArray/Main.scala
@@ -0,0 +1,12 @@
+object Main extends App{
+    val nums = Seq(1,2,3)
+    for (n <- nums ) println(n)
+    val people = List(
+        "Bill", 
+        "Candy", 
+        "Karen", 
+        "Leo", 
+        "Regina"
+    )
+    for (p <- people) println(p)
+}
