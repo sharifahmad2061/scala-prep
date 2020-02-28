40d1ba1947575cf4bffe7df93b027d0fffc8bcaa
sharif ahmad <sharifahmad2061@gmail.com>
2020-02-28 15:39:46 +0500

    a block of code can also be used after a yield statement

--- 5yield/Main.scala
+++ 5yield/Main.scala
@@ -6,4 +6,17 @@ object Main extends App{
     // val newnewnums = newnums.foreach{case _ => _ ** 2}
     // println("newnewnums>" + newnewnums.mkString(" "))
     // for(n <- nums )println(n)
+    val names = List("_adam", "_david", "_frank")
+    val newnames = for(name <- names) yield{
+        val namewithoutunderscore = name.drop(1)
+        val capitalname = namewithoutunderscore.capitalize
+        capitalname
+    }
+    newnames.foreach(print)
+    println()
+    (
+        for(name <- names) 
+            yield name.drop(1).toUpperCase()
+    )
+    .foreach(println)
 }
