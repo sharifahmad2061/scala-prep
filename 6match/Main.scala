13a17edb78e4f62cc33c950e5f56d8391fb5600b
sharif ahmad <sharifahmad2061@gmail.com>
2020-03-02 10:01:43 +0500

    learnt ins and outs of match
    - variable defined once cannot be defined again, with the same name but different type.
    - variable of type string cannot be assigned a boolean value.

--- 6match/Main.scala
+++ 6match/Main.scala
@@ -3,8 +3,19 @@ object Main extends App {
     case true  => "you said true"
     case false => "you said false"
   }
+  def isTrue(a: Any): Boolean = a match {
+    case 0 | "" => false
+    case _      => true
+  }
   var ret = convertBooleanToString(true)
   println(ret)
   ret = convertBooleanToString(false)
   println(ret)
+  println("---")
+  var ret1: Boolean = isTrue("hello")
+  println(ret1)
+//   ret = 
+  println(isTrue(0))
+  ret1 = isTrue("")
+  println(ret1)
 }
