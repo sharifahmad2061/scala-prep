dcea4e57178745bed8a8445b22c5ea71ae53d908
sharif ahmad <sharifahmad2061@gmail.com>
2020-03-02 11:18:12 +0500

    learned about scala tuples
    - scala tuples are 1 indexed
    - foreach passes each key, value pair of a map as a tuple to lambda

--- 4seqListArray/Main.scala
+++ 4seqListArray/Main.scala
@@ -1,27 +1,30 @@
-object Main extends App{
-    val nums = Seq(1,2,3)
-    for (n <- nums ){
-        println(n)
-    }
-    val people = List(
-        "Bill", 
-        "Candy", 
-        "Karen", 
-        "Leo", 
-        "Regina"
-    )
-    for (p <- people){
-        println(p)
-    }
-    val ratings = Map(
-        "Lady in the Water"  -> 3.0, 
-        "Snakes on a Plane"  -> 4.0, 
-        "You, Me and Dupree" -> 3.5
-    )
-    ratings.foreach{
-        case (movie, rating) => println(s"key: ${movie}, value: ${rating}")
-    }
-    for ( (name, rating) <- ratings){
-        println(s"movie: ${name}, rating: ${rating}")
-    }
+object Main extends App {
+  val nums = Seq(1, 2, 3)
+  for (n <- nums) {
+    println(n)
+  }
+  val people = List(
+    "Bill",
+    "Candy",
+    "Karen",
+    "Leo",
+    "Regina"
+  )
+  for (p <- people) {
+    println(p)
+  }
+  val ratings = Map(
+    "Lady in the Water" -> 3.0,
+    "Snakes on a Plane" -> 4.0,
+    "You, Me and Dupree" -> 3.5
+  )
+  ratings.foreach((obj) => {
+    println(s"key: ${obj._1}, value: ${obj._2}")
+  })
+  ratings.foreach {
+    case (movie, rating) => println(s"key: ${movie}, value: ${rating}")
+  }
+  for ((name, rating) <- ratings) {
+    println(s"movie: ${name}, rating: ${rating}")
+  }
 }
